99af7c70667e19270e4980367726a662bd08309a
Bruna Pinos <bruna.pinos@gmail.com>
2018-12-16 01:04:04 -0200

    Makes the bot works

--- duentebot.py
+++ duentebot.py
@@ -2,10 +2,6 @@
 
 # bot = telepot.Bot('678582209:AAGdYnxxV3SkRKzWm8BBR8ur4ozAcozzxQo')
 # bot.getMe()
-
-# from pprint import pprint
-# response = bot.getUpdates()
-# pprint(response)
 import sys
 import asyncio
 from functools import reduce
@@ -16,22 +12,12 @@ from telepot.aio.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.aio.delegate import (
     per_chat_id, create_open, pave_event_space, include_callback_query_chat_id)
-
-"""
-$ python3.5 votea.py <token>
-
-Add the bot to a group. Then send a command `/vote` to the group. The bot will
-organize a ballot.
-
-When all group members have cast a vote or when time expires (30 seconds),
-it will announce the result. It demonstrates how to use the scheduler to
-generate an expiry event after a certain delay.
-
-It statically captures callback query according to the originating chat id.
-This is the chat-centric approach.
-"""
+import requests
+import json
+import random
 
 votes = dict()
+draw = []
 
 class VoteCounter(telepot.aio.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
@@ -48,9 +34,6 @@ class VoteCounter(telepot.aio.helper.ChatHandler):
             self._expired_event = None
             self._member_count = None
 
-        # Catch _vote_expired event
-        self.router.routing_table['_vote_expired'] = self.on__vote_expired
-
     async def on_chat_message(self, msg):
         content_type, chat_type, chat_id = glance(msg)
 
@@ -58,24 +41,41 @@ class VoteCounter(telepot.aio.helper.ChatHandler):
             print('Not a text message.')
             return
 
-        if msg['text'] != '/vote':
-            print('Not /vote')
-            return
-
-        if self._ballot_box is not None:
-            await self.sender.sendMessage('Voting still in progress')
-        else:
+        if msg['text'] == '/start':
             await self._init_ballot()
 
-    def _count_votes(self):
-        yes = reduce(lambda a,b: a+(1 if b=='yes' else 0), self._ballot_box.values(), 0)
-        no = reduce(lambda a,b: a+(1 if b=='no' else 0), self._ballot_box.values(), 0)
-        return yes, no, self._member_count-yes-no
+        elif msg['text'] == '/startdraw':
+            result = self._draw()
+            await self._close_ballot()
+            await self.sender.sendMessage('O sorteio foi realizado!')
+
+        else:
+            print('Not a command')
+            return
+
+            
+    def _draw(self):
+
+        copy = draw.copy()
+
+        for user in draw:
+            chosen = random.choice(copy)
+            while chosen == user:
+                chosen = random.choice(copy)
+            
+            data = requests.get(
+                'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'
+                .format(TOKEN, user["id"]
+                    , "Você tirou " + chosen["first_name"] + " " + chosen["last_name"] + " (@" + chosen["username"] + ")")
+            ).json()
+
+            copy.remove(chosen)
+
+        return 
 
     async def _init_ballot(self):
         keyboard = InlineKeyboardMarkup(inline_keyboard=[[
                        InlineKeyboardButton(text='Yes', callback_data='yes'),
-                       InlineKeyboardButton(text='Nah!!!!', callback_data='no'),
                    ]])
         sent = await self.sender.sendMessage("Let's Vote ...", reply_markup=keyboard)
 
@@ -85,10 +85,9 @@ class VoteCounter(telepot.aio.helper.ChatHandler):
         self._keyboard_msg_ident = message_identifier(sent)
         self._editor = telepot.aio.helper.Editor(self.bot, self._keyboard_msg_ident)
 
-        # Generate an expiry event 30 seconds later
-        self._expired_event = self.scheduler.event_later(10, ('_vote_expired', {'seconds': 10}))
 
     async def _close_ballot(self):
+        self._expired_event = self.scheduler.event_later(1, ('_vote_expired', {'seconds': 1}))
         self.scheduler.cancel(self._expired_event)
 
         await self._editor.editMessageReplyMarkup(reply_markup=None)
@@ -103,19 +102,31 @@ class VoteCounter(telepot.aio.helper.ChatHandler):
         if from_id in self._ballot_box:
             await self.bot.answerCallbackQuery(query_id, text='You have already voted %s' % self._ballot_box[from_id])
         else:
+            data = requests.get(
+                'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'
+                .format(TOKEN, from_id, "Agora você está participando")
+            ).json()
+
+            user = {}
+
+            user["username"] = ""
+            user["first_name"] = ""
+            user["last_name"] = ""
+            user["id"] = None
+
+            try:
+                user["username"] = data["result"]["chat"]["username"]
+                user["first_name"] = data["result"]["chat"]["first_name"]
+                user["last_name"] = data["result"]["chat"]["last_name"]
+            except Exception as exception:
+                print(exception)
+
+            user["id"] = from_id
+            draw.append(user)
+            
             await self.bot.answerCallbackQuery(query_id, text='Ok')
             self._ballot_box[from_id] = query_data
 
-        # Announce results if everyone has voted.
-        if len(self._ballot_box) >= self._member_count:
-            result = self._count_votes()
-            await self._close_ballot()
-            await self.sender.sendMessage('Everyone has voted:\nYes: %d\nNo: %d\nSilent: %d' % result)
-
-    async def on__vote_expired(self, event):
-        result = self._count_votes()
-        await self._close_ballot()
-        await self.sender.sendMessage('Time is up:\nYes: %d\nNo: %d\nSilent: %d' % result)
 
     def on_close(self, ex):
         global votes
